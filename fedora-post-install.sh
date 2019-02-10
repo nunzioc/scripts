3a9807a188f6454f7e2f4d73668d678b2ab35fe7
nunzio cicone <ncicone@localhost.localdomain>
2019-02-10 08:03:54 -0500

    init

---
+++ fedora-post-install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# install rpm fusion
+sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+# install linux utils
+sudo dnf install -y util-linux-user
+
+# install fish shell
+sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/shells:fish:release:3/Fedora_29/shells:fish:release:3.repo
+sudo dnf install -y fish
+
+# set fish as default shell
+chsh -s `which fish`
+
+# install video codecs
+sudo dnf install -y compat-ffmpeg28 ffmpeg-libs
