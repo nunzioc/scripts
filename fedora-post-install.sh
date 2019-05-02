fb935fcdeec46829b5cd4b6e19c85938589792fe
Nunzio Cicone <nunziocicone@localhost.localdomain>
2019-05-02 17:40:21 -0400

    added installing configs

--- fedora-post-install.sh
+++ fedora-post-install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# run from home
+cd ~
+
 # install rpm fusion
 sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
@@ -7,10 +10,16 @@ sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-r
 sudo dnf install -y util-linux-user
 
 # install software
-sudo dnf install -y fish qutebrowser fzf ranger steam
+sudo dnf install -y fish qutebrowser fzf ranger steam vim
 
 # set fish as default shell
 chsh -s `which fish`
 
 # install video codecs
 sudo dnf install -y compat-ffmpeg28 ffmpeg-libs
+
+# set up configs
+mkdir .configrepo
+git clone https://github.com/nunzioc/config.git ~/.configrepo
+alias config='/usr/bin/git --git-dir=$HOME/.configrepo/ --work-tree=$HOME'
+config checkout -f
