66c2b710afa018a5374d90081e7f013ea11526ef
Nunzio Cicone <nunziocicone@gmail.com>
2021-12-02 12:13:15 -0500

    don't show untracked files

--- fedora-post-install.sh
+++ fedora-post-install.sh
@@ -35,4 +35,5 @@ cd ~
 mkdir .configrepo
 git clone --bare https://github.com/nunzioc/config.git ~/.configrepo
 alias config='/usr/bin/git --git-dir=$HOME/.configrepo/ --work-tree=$HOME'
+config config --local status.showUntrackedFiles no
 config checkout 
