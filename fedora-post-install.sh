bbce7f5c3197da88e98825c1f2a39ecd65e6894d
Nunzio <nunzarius@gmail.com>
2023-10-24 08:13:18 -0400

    update script

--- fedora-post-install.sh
+++ fedora-post-install.sh
@@ -6,30 +6,15 @@ cd ~
 # install rpm fusion
 sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-# install linux utils
-sudo dnf install -y util-linux-user
-
 # install software
-sudo dnf install -y fish fzf ranger steam vim tmux tmuxinator
+sudo dnf install -y util-linux-user fish vim micro vlc
 
 # set fish as default shell
 chsh -s `which fish`
 
-# install video codecs
-sudo dnf install -y compat-ffmpeg28 ffmpeg-libs
-
-# set up source
-mkdir source
-# set up copy paste
-cd source
-sudo dnf install -y wayland-devel meson
-git clone https://github.com/bugaevc/wl-clipboard.git
-cd wl-clipboard
-meson build
-cd build
-ninja
-sudo install ninja
-cd ~
+# set up common dirs
+mkdir source projects downloads
+rm -rf Downloads Music Templates Videos Desktop Public Pictures Documents
 
 # set up configs
 mkdir .configrepo
